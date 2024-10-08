849e6320314abf491f4d69f1d2ad7341159975d3
Gurtaj <gurtajs98@gmail.com>
2024-10-08 15:56:33 +0100

    working on home page

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+     <header>Hi, I am Gurtaj</header>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+export default function Navbar() {
+  return (
+    <ul className="flex justify-around">
+      <li>Home</li>
+      <li>Projects</li>
+      <li>About</li>
+    </ul>
+  );
+}
